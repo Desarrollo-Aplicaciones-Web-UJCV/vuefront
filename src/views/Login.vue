77bfe94da2e529cb88544fc4a2f869c9a31ed826
francklinmarroq <francklin.marroquin@outlook.com>
2021-12-08 15:41:25 -0600

    min configs

---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Card>ssdasddadsa</Card>
+
+
+    <div>asldñañsldk</div>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  component:{
+  },
+  setup() {
+    
+  },
+})
+</script>
+                                                                                                                                                                         
